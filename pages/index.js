a59d7a9ef47ae27254f44364da3c1ed5b5d4656d
Uthman Momen <uthman.momen2000@berkeley.edu>
2022-11-25 12:02:22 -0800

    small update to index page

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Nav from "../components/Nav.js";
+import Hero from '../components/Hero';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Atfal USA</title>
-        <meta name="description" content="" />
-      </Head>
-      
-      <Nav/>
-
-      <main className={styles.main}>
-      </main>
-
-    </div>
+    <>
+      <Header
+      data={{
+        title: "Atfal USA",
+        description: "Atfal Data",
+      }}
+    />
+    <Nav />
+    <main className="mainContent">
+      <Hero text={"Atfal Data"}/>
+    </main>
+    <Footer />
+    </>
   )
 }
