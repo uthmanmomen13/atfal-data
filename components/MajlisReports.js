643581741e7b8771327a412aa763234352f1ced3
Uthman Momen <uthman.momen2000@berkeley.edu>
2022-11-25 11:43:39 -0800

    fixed table display for report data

--- components/MajlisReports.js
+++ components/MajlisReports.js
@@ -4,69 +4,69 @@ import Nav from "../components/Nav.js";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import styles from "../styles/RegionalTable.module.css"
-import Link from "next/link.js";
+import styles from "../styles/Table.module.css"
 const MAJLIS_INDEX = 4;
 
 
-export default function MajlisReports({headerData, majlisList}) {
+export default function MajlisReports({majlisList}) {
+  sortByMonth(majlisList)
     let list = getRows(majlisList)
 
     function getRows(data_) {
         let allRows = data_.map((entry, i) => 
         {
             return ( 
-        <tr key={`${entry[0]}_Row`} >
-          <td>{entry[0]}</td>
-          <td>{entry[1]}</td>
-          <td>{entry[2]}</td>
-          <td>{entry[3]}</td>
-          <td>{entry[4]}</td>
-          <td>{entry[5]}</td>
-          <td>{entry[6]}</td>
-          <td>{entry[7]}</td>
-          <td>{entry[8]}</td>
-          <td>{entry[9]}</td>
-          <td>{entry[10]}</td>
-          <td>{entry[11]}</td>
-          <td>{entry[12]}</td>
-          <td>{entry[13]}</td>
-          <td>{entry[14]}</td>
-          <td>{entry[15]}</td>
-          <td>{entry[16]}</td>
-          <td>{entry[17]}</td>
-          <td>{entry[18]}</td>
-          <td>{entry[19]}</td>
+        <tr key={`${entry[0]}_Row`} className={styles.tr} >
+          <td >{entry[0]}</td>
+          <td >{entry[1]}</td>
+          <td >{entry[2]}</td>
+          <td >{entry[3]}</td>
+          <td >{entry[4]}</td>
+          <td >{entry[5]}</td>
+          <td >{entry[6]}</td>
+          <td >{entry[7]}</td>
+          <td >{entry[8]}</td>
+          <td >{entry[9]}</td>
+          <td >{entry[10]}</td>
+          <td >{entry[11]}</td>
+          <td >{entry[12]}</td>
+          <td >{entry[13]}</td>
+          <td >{entry[14]}</td>
+          <td >{entry[15]}</td>
+          <td >{entry[16]}</td>
+          <td >{entry[17]}</td>
+          <td >{entry[18]}</td>
+          <td >{entry[19]}</td>
           
         </tr> 
       )})
     return (
         <>
-            <thead>
+            <thead >
                 <tr>
                     <br/><br/>
                 </tr>
                 
-                <tr>
-                <td>Timestamp</td>
-                <td>Month</td>
-                <td>Name</td>
-                <td>Majlis</td>
-                <td>Jamaat role</td>
-                <td >Held amila meeting</td>
-                <td >Meeting minutes</td>
-                <td>Parents contacted</td>
-                <td>Atfal classes held</td>
-                <td>Average attendance</td>
-                <td>Atfal who read book pages</td>
-                <td>Khidmat-e-Khalq activities held</td>
-                <td>Waqar-e-Amal activities held</td>
-                <td>Atfal participants in Waqar-e-Amal</td>
-                <td>Held a sports event</td>
-                <td>Atfal participants in sports</td>
-                <td>Atfal encouraged to write for Al-Bashir</td>
-                <td>Reminded parents to subscribe to Atfal Digest</td>
-                <td>Waqf-e-Nau who attended class</td>
+                <tr className={styles.tr} >
+                <th >Timestamp</th>
+                <th >Month</th>
+                <th >Name</th>
+                <th >Majlis</th>
+                <th >Jamaat role</th>
+                <th >Held amila meeting</th>
+                <th style={{width:"10%"}}>Meeting minutes</th>
+                <th >Parents contacted</th>
+                <th >Atfal classes held</th>
+                <th >Average attendance</th>
+                <th >Atfal who read book pages</th>
+                <th >Khidmat-e-Khalq activities held</th>
+                <th >Waqar-e-Amal activities held</th>
+                <th >Atfal participants in Waqar-e-Amal</th>
+                <th >Held a sports event</th>
+                <th >Atfal participants in sports</th>
+                <th >Atfal encouraged to write for Al-Bashir</th>
+                <th >Reminded parents to subscribe to Atfal Digest</th>
+                <th >Waqf-e-Nau who attended class</th>
 
                 </tr>
           </thead>
@@ -78,11 +78,21 @@ export default function MajlisReports({headerData, majlisList}) {
     }
 
     return (
-        <Col style={{ width: "100%", paddingLeft: "50px", paddingRight: "50px" }}>
-        <table className="table table-hover">
+        <Col style={{ paddingLeft: "50px", paddingRight: "50px" }}>
+        <table style={{width: "150%"}}>
           {list}
-
         </table>
+        
         </Col>
     )
+}
+
+function sortByMonth(arr) {
+  
+  var months = ["January", "February", "March", "April", "May", "June",
+  	        "July", "August", "September", "October", "November", "December"];
+  arr.sort(function(a, b){
+      return months.indexOf(a[1])
+           - months.indexOf(b[1]);
+  });
 }
